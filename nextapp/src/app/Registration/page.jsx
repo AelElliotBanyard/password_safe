db6cd15b47c2cb38a1eca702d051112f64495507
Evan <evan.lueber@stud.kbw.ch>
2023-11-22 08:30:00 +0100

    props added to InputRegistration

--- nextapp/src/app/Registration/page.jsx
+++ nextapp/src/app/Registration/page.jsx
@@ -1,8 +1,14 @@
 "use client";
+import Input from "../../components/InputRegistration";
 
 const page = () => {
   return (
-    <div></div>
+    <div className="flex flex-col gap-20 min-h-screen w-screen items-center justify-center bg-black font-serif">
+      <p className="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-5 text-5xl text-white font-bold font-serif ">
+        Registrieren
+      </p>
+      <Input type="email" />
+    </div>
   )
 }
 
--- nextapp/src/app/components/InputRegistration.jsx
+++ nextapp/src/app/components/InputRegistration.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
-const InputRegistration = ({ type, placeholder, onChange }) => {
+const InputRegistration = ({ type, placeholder, onChange, value }) => {
   return (
     <div>
       <input
         type={type}
         placeholder={placeholder}
         onChange={onChange}
+        value={value}
         className="h-8 outline-none border-b border-white bg-black text-white w-full"
       />
     </div>

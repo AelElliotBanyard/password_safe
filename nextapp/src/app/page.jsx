ed7f11351a02bd65c6e7b6e9b77be66397460568
Evan <evan.lueber@stud.kbw.ch>
2023-11-22 08:09:23 +0100

    loginpage done, registrationpage created

---
+++ nextapp/src/app/page.jsx
@@ -0,0 +1,19 @@
+"use client";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col gap-20 min-h-screen w-screen items-center justify-center bg-black font-serif">
+      <div className="w-max">
+        <p className="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-5 text-5xl text-white font-bold font-serif ">
+          PASSWORD SAFE
+        </p>
+      </div>
+      <div className="flex flex-col items-center gap-5 w-1/4">
+        <input className="h-8 outline-none border-b border-white bg-black text-white w-full" type="email" placeholder="Enter your email" />
+        <input className="h-8 outline-none border-b border-white bg-black text-white w-full" type="password" placeholder="Enter your password" />
+        <button className="h-8 border-white border text-white rounded w-full" type="submit">Login</button>
+        <p> Noch kein Konto? <a className="text-blue-500 border-b border-blue-500" href="/Registration">Hier registrieren</a></p>
+      </div>
+    </div>
+  );
+}

91c291e95bf2166ffc1245235b62868ceca95bc3
Evan <evan.lueber@stud.kbw.ch>
2023-11-22 08:58:24 +0100

    typo in import fixed

--- nextapp/src/app/page.jsx
+++ nextapp/src/app/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import Input from "../components/InputRegistration";
+import Input from "./components/InputRegistration";
 
 export default function Home() {
   const [email, setEmail] = React.useState("");

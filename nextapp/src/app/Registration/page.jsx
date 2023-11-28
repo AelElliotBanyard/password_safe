bac6b164114f26607c9364e0bd1fb133a305fb05
Ael Elliot Banyard <64694151+AelElliotBanyard@users.noreply.github.com>
2023-11-28 10:35:35 +0100

    added api to registration

--- nextapp/src/app/Registration/page.jsx
+++ nextapp/src/app/Registration/page.jsx
@@ -2,33 +2,48 @@
 import Input from "../components/Input";
 import React from "react";
 import { usePathname, useRouter } from "next/navigation";
+import api from "@/utils/api";
 
 const page = () => {
-  const[firstname, setFirstname] = React.useState("");
-  const[lastname, setLastname] = React.useState(""); 
+  const [firstname, setFirstname] = React.useState("");
+  const [lastname, setLastname] = React.useState("");
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
   const [conPassword, setConPassword] = React.useState("");
-  const router = useRouter()
+  const router = useRouter();
 
   function PasswordCheck() {
-    if(password === conPassword) {
+    if (password === conPassword) {
       return true;
     } else {
       alert("Passwords do not match");
       return false;
     }
   }
-  
 
-  function Register() {
-      if(firstname === "" || lastname === "" || email === "" || password === "" || conPassword === "") {
-        alert("Please fill in all fields");
-      } else if(!PasswordCheck()) {
-        alert("Passwords do not match");
-      } 
-        else {
-        router.push("/");
+  async function Register() {
+    if (
+      firstname === "" ||
+      lastname === "" ||
+      email === "" ||
+      password === "" ||
+      conPassword === ""
+    ) {
+      alert("Please fill in all fields");
+    } else if (!PasswordCheck()) {
+      alert("Passwords do not match");
+    } else {
+      const register = await api.register({
+        firstname,
+        lastname,
+        email,
+        password,
+      });
+      if (!register) {
+        alert("Something went wrong");
+        return;
+      }
+      router.push("/");
     }
   }
 
@@ -36,16 +51,53 @@ const page = () => {
     <div className="flex flex-col gap-14 min-h-screen items-center justify-center w-screen font-serif">
       <p className="text-5xl text-white font-bold font-serif">Registration</p>
       <div className="flex flex-col justify-center items-center gap-5 w-1/4 ">
-        <Input type="text" value={firstname} placeholder={"Firstname"} onChange={(e) => setFirstname(e.target.value)}/>
-        <Input type="text" value={lastname} placeholder={"Lastname"} onChange={(e) => setLastname(e.target.value)}/>
-        <Input type="email" value={email} placeholder={"Email"} onChange={(e) => setEmail(e.target.value)}/>
-        <Input type="password" value={password} placeholder={"Password"} onChange={(e) => setPassword(e.target.value)}/>
-        <Input type="password" value={conPassword} placeholder={"Confirm Password"} onChange={(e) => setConPassword(e.target.value)}/>
-        <button className="h-8 border-white border text-white rounded w-full hover:bg-white hover:text-black" type="submit" onClick={Register}>Register</button>
-        <p> Already have an account? <a className="text-blue-500 border-b border-blue-500" href="/">Click to Login</a></p>
+        <Input
+          type="text"
+          value={firstname}
+          placeholder={"Firstname"}
+          onChange={(e) => setFirstname(e.target.value)}
+        />
+        <Input
+          type="text"
+          value={lastname}
+          placeholder={"Lastname"}
+          onChange={(e) => setLastname(e.target.value)}
+        />
+        <Input
+          type="email"
+          value={email}
+          placeholder={"Email"}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          type="password"
+          value={password}
+          placeholder={"Password"}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Input
+          type="password"
+          value={conPassword}
+          placeholder={"Confirm Password"}
+          onChange={(e) => setConPassword(e.target.value)}
+        />
+        <button
+          className="h-8 border-white border text-white rounded w-full hover:bg-white hover:text-black"
+          type="submit"
+          onClick={Register}
+        >
+          Register
+        </button>
+        <p>
+          {" "}
+          Already have an account?{" "}
+          <a className="text-blue-500 border-b border-blue-500" href="/">
+            Click to Login
+          </a>
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;

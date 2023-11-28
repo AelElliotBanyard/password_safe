1edb04909d57eabcd360dd4d60ec3af732c5aff0
Ael Elliot Banyard <64694151+AelElliotBanyard@users.noreply.github.com>
2023-11-28 19:23:30 +0100

    component for new entries

--- nextapp/src/app/components/NewEntry.jsx
+++ nextapp/src/app/components/NewEntry.jsx
@@ -1,9 +1,84 @@
-import React from 'react'
+import React from "react";
+import InputPassword from "./InputPassword";
+import EntryButtons from "./EntryButtons";
 
-const NewEntry = () => {
+const NewEntry = ({ setShowEmpty, setShowEditor, setShowEntry }) => {
+  const [title, setTitle] = React.useState("");
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [url, setUrl] = React.useState("");
+  const [description, setDescription] = React.useState("");
   return (
-    <div>NewEntry</div>
-  )
-}
+    <div className="h-full bg-[#07111B] bg-opacity-30 rounded-lg flex flex-col justify-between gap-5 shadow-lg shadow-[#0C1F31] p-5 ">
+      <p className="text-3xl text-white flex justify-center items-center font-serif ">
+        {title}
+      </p>
+      <div className="flex flex-col text-lg justify-center items-center ">
+        <div className="flex flex-col w-full gap-8">
+          <div className="flex flex-col gap-2 w-full border-b-2 border-b-[#07111B]">
+            <p className="text-white opacity-75">Title</p>
+            <input
+              type="text"
+              className="text-white text-xl outline-none bg-transparent focus:outline-none"
+              value={title}
+              onChange={(e) => {
+                setTitle(e.target.value);
+              }}
+            />
+          </div>
+          <div className="flex flex-col gap-2 w-full border-b-2 border-b-[#07111B]">
+            <p className="text-white opacity-75">Username</p>
+            <input
+              type="text"
+              className="text-white text-xl outline-none bg-transparent focus:outline-none"
+              value={username}
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+            />
+          </div>
+          <div className="flex flex-col gap-2 w-full border-b-2 border-b-[#07111B] ">
+            <p className="text-white opacity-75">Password</p>
+            <InputPassword
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </div>
+          <div className="flex flex-col gap-2 w-full border-b-2 border-b-[#07111B] ">
+            <p className="text-white opacity-75">URL</p>
+            <input
+              type="text"
+              className="text-white text-xl outline-none bg-transparent focus:outline-none"
+              value={url}
+              onChange={(e) => {
+                setUrl(e.target.value);
+              }}
+            />
+          </div>
+          <div className="flex flex-col gap-2 w-full border-b-2 border-b-[#07111B] ">
+            <p className="text-white opacity-75">Description</p>
+            <input
+              type="text"
+              className="text-white text-xl outline-none bg-transparent focus:outline-none"
+              value={description}
+              onChange={(e) => {
+                setDescription(e.target.value);
+              }}
+            />
+          </div>
+        </div>
+      </div>
+      <div className=" flex justify-center items-center">
+        <EntryButtons
+          setShowEmpty={setShowEmpty}
+          setShowEditor={setShowEditor}
+          setShowEntry={setShowEntry}
+        />
+      </div>
+    </div>
+  );
+};
 
-export default NewEntry
+export default NewEntry;

bb2e36344623aea0cce868e854e7aa213c51671c
Ael Elliot Banyard <64694151+AelElliotBanyard@users.noreply.github.com>
2023-11-28 19:23:18 +0100

    custom input for passwords

---
+++ nextapp/src/app/components/InputPassword.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { RiEyeLine, RiEyeCloseLine } from "react-icons/ri";
+
+const InputPassword = ({ value, onChange }) => {
+  const [show, setShow] = React.useState(false);
+  return (
+    <div className="flex flex-row gap-0 w-full">
+      <input
+        type={show ? "text" : "password"}
+        value={value}
+        onChange={onChange}
+        className="text-white text-xl outline-none bg-transparent focus:outline-none flex-grow"
+      />
+      {show ? (
+        <RiEyeCloseLine
+          className="text-white text-xl cursor-pointer"
+          onClick={() => setShow(!show)}
+        />
+      ) : (
+        <RiEyeLine
+          className="text-white text-xl cursor-pointer"
+          onClick={() => setShow(!show)}
+        />
+      )}
+    </div>
+  );
+};
+
+export default InputPassword;
